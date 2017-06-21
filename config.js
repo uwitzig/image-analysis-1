088ffe79bd5e204163cabf445464acebedf93f6d
Klaus-Peter Schlotter <klaus-peter.schlotter@de.ibm.com>
2017-06-21 13:19:58 +0200

    config.js

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ var config = {
 	watson: {
 		visual_recognition: {
 			url:'https://gateway-a.watsonplatform.net/visual-recognition/api',
-			api_key: '18c455c6b24c4b22bda146aa64d3d56b62de73d0',
+			api_key: '',
 			version_date: '2016-05-20'
 		},
 		text_to_speech: {
 			url:'https://stream.watsonplatform.net/text-to-speech/api',
-			username: '79d5a9e5-3ad9-4509-8249-0be44e4caf68',
-			password: 'N62xic7WCaHQ',
+			username: '',
+			password: '',
 			version: 'v1'
 		},
 		language_translator: {
 			url: 'https://gateway.watsonplatform.net/language-translator/api',
-			username: '5a2080fc-5de1-41ca-bd5a-196855abca63',
-			password: 'xsoCsBqaa1tu',
+			username: '',
+			password: '',
 			version: 'v2'
 		}
 	}
